f521bd51da6ce5fdf616ea64a3b9c581c18a9be8
dragon-src <72925570+dragon-src@users.noreply.github.com>
2021-01-28 19:13:27 +0000

    Added unit tests for the Obstacle class

---
+++ tests/src/caseus/dragonboattesting/tests/unit/ObstacleTest.java
@@ -0,0 +1,55 @@
+package caseus.dragonboattesting.tests.unit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import com.dragonboat.game.Obstacle;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ObstacleTest {
+	private Obstacle obstacle;
+
+	@Mock
+	Texture tex;
+
+	int damage = 10;
+	int xPosition = 10;
+	int yPosition = 10;
+	int width = 10;
+	int height = 10;
+
+	@BeforeEach
+	public void setup() {
+		obstacle = new Obstacle(damage, xPosition, yPosition, width, height, tex);
+	}
+
+	@Test
+	void testGetX() {
+		Assertions.assertEquals(xPosition, obstacle.getX());
+	}
+
+	@Test
+	void testGetY() {
+		Assertions.assertEquals(yPosition, obstacle.getY());
+	}
+
+	@Test
+	void testMove() {
+		float moveAmount = 20;
+		int backgroundOffset = 0;
+		float[] expectedPosition = {xPosition, yPosition - moveAmount};
+		obstacle.Move(moveAmount, backgroundOffset);
+		float[] finalPosition = {obstacle.getX(), obstacle.getY()};
+
+		Assertions.assertArrayEquals(expectedPosition, finalPosition);
+	}
+}
